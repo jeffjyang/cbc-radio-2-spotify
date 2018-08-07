cb042d23af929feadc8a574b52494fa7c51439b5
Jeff Yang <jeffjieyang@gmail.com>
2018-08-06 18:59:58 -0700

    add config

---
+++ src/config.py
@@ -0,0 +1,26 @@
+# Spotify credentials
+USERNAME = "<Spotify username>"
+CLIENT_ID = "<Spotify application client id>"
+CLIENT_SECRET = "<Spotify application client secret>"
+SCOPE = "playlist-modify-public"
+REDIRECT_URI = "http://localhost"
+
+UTC_OFFSET = -7
+
+# Playlists
+PLAYLISTS = [
+    "<Spotify playlist_id>",
+    "<Spotify playlist_id>",
+    "<Spotify playlist_id>",
+    "<Spotify playlist_id>",
+    "<Spotify playlist_id>",
+    "<Spotify playlist_id>",
+    "<Spotify playlist_id>",
+    "<Spotify playlist_id>",
+    "<Spotify playlist_id>",
+    "<Spotify playlist_id>"
+]
+
+# AWS credentials
+AWS_ACCESS_KEY_ID = "<AWS access key id>"
+AWS_SECRET_ACCESS_KEY = "<AWS secret access key>"
