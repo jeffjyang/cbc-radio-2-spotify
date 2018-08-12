e49609dca5285fafd71b73f6b5e27f3dfe3776d9
Jeff Yang <jeffjieyang@gmail.com>
2018-08-11 18:57:56 -0700

    update config

--- src/config.py
+++ src/config.py
@@ -2,12 +2,11 @@
 USERNAME = "<Spotify username>"
 CLIENT_ID = "<Spotify application client id>"
 CLIENT_SECRET = "<Spotify application client secret>"
+
 SCOPE = "playlist-modify-public"
 REDIRECT_URI = "http://localhost"
 
-UTC_OFFSET = -7
-
-# Playlists
+# Spotify playlists
 PLAYLISTS = [
     "<Spotify playlist_id>",
     "<Spotify playlist_id>",
@@ -21,6 +20,12 @@ PLAYLISTS = [
     "<Spotify playlist_id>"
 ]
 
+# Timezone
+UTC_OFFSET = -7
+
 # AWS credentials
 AWS_ACCESS_KEY_ID = "<AWS access key id>"
 AWS_SECRET_ACCESS_KEY = "<AWS secret access key>"
+
+AWS_S3_BUCKET = "<AWS S3 bucket name>" 
+
